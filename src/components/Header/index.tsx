bb47c36f167055d7c995ca894a47a2c54ba308ba
Beatriz <beatriz.lira@skeelo.app>
2023-05-30 23:51:19 -0300

    feat: update header component styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,3 @@
-import headerBackgroundLeftImg from "@/assets/header-background-efect-left.svg";
-import headerBackgroundRightImg from "@/assets/header-background-efect-right.svg";
 import logoImg from "@/assets/logo.svg";
 
 import Image from "next/image";
@@ -10,10 +8,6 @@ export const Header = () => {
   return (
     <header className={styles.container}>
       <Image src={logoImg} alt="" priority className={styles.logo} />
-      {/* <div className={styles["background-images"]}>
-        <Image src={headerBackgroundLeftImg} alt="" />
-        <Image src={headerBackgroundRightImg} alt="" />
-      </div> */}
     </header>
   );
 };
