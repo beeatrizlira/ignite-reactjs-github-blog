a7d2f329be8aa95b8472282f5cea44dc050a18ae
beeatrizlira <beatriz.rolim01@outlook.com>
2023-06-03 18:07:16 -0300

    feat: add getPosts and getPost api calls

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,4 +7,15 @@ export class GithubBlogAPI {
     const endpoint = "users/beeatrizlira";
     return Service.get(endpoint);
   };
+
+  getPosts = async (): Promise<AxiosResponse<any, any>> => {
+    const endpoint =
+      "search/issues?q=repo:beeatrizlira/ignite-reactjs-github-blog";
+    return Service.get(endpoint);
+  };
+
+  getPost = async (postNumber: number): Promise<AxiosResponse<any, any>> => {
+    const endpoint = `repos/beeatrizlira/ignite-reactjs-github-blog/issues/${postNumber}`;
+    return Service.get(endpoint);
+  };
 }
